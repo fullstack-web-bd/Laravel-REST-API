fd7862c9b8678df87060389a74a307811684eeb7
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-07-13 20:20:41 +0600

    Day 01 - Migrations, Models

---
+++ database/migrations/2022_07_13_122221_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->float('price')->default(0);
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('user_id')->comment('User who created this product');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    // dd(Product::all());
+    return view('welcome');
+});
